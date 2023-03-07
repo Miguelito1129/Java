ad1ab6f6e33d1a0be055d6ae59b188eba89eb6c9
Miguelito1129 <mdmm1100@gmail.com>
2023-03-06 22:29:06 -0500

    Agregado

---
+++ Recursividad.java
@@ -0,0 +1,17 @@
+public class PotenciaRecursiva {
+
+    public static void main(String[] args) {
+        int base = 2;
+        int exponente = 5;
+        int resultado = potencia(base, exponente);
+        System.out.println(base + " elevado a " + exponente + " es igual a " + resultado);
+    }
+
+    public static int potencia(int base, int exponente) {
+        if (exponente == 0) {
+            return 1;
+        } else {
+            return base * potencia(base, exponente - 1);
+        }
+    }
+}
